4f68bd392b32db55dd022049571908e7022e2916
choiheebak <149461285+choiheebak@users.noreply.github.com>
2024-10-19 15:55:42 +0900

    Add files via upload

--- mt_0main.py
+++ mt_0main.py
@@ -2,12 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import sqlite3
-import m_ma_1all
+# import m_ma_1all
 import mt_so_1all
-import m_bb_1all
-import m_bk_1all
-import m_dt_1all
-import m_jo_1all
+# import mt_so_1all
+# import mt_so_1all
+# import mt_so_1all
+# import mt_so_1all
 from streamlit_option_menu import option_menu
 from streamlit_navigation_bar import st_navbar
 # 한글폰트
@@ -49,7 +49,7 @@ if choice == "회차 조회":
                     break
         fr.close
 
-        m_ma_1all.Crawler(year,count,'s') 
+        # m_ma_1all.Crawler(year,count,'s') 
 
     elif pagema == "야구 승1패": 
 
@@ -67,7 +67,7 @@ if choice == "회차 조회":
             count.append(p2)
 
         con.close()    
-        m_ma_1all.Crawler(year,count,'b')
+        # m_ma_1all.Crawler(year,count,'b')
 
     elif pagema == "농구 승5패": 
 
@@ -86,7 +86,7 @@ if choice == "회차 조회":
 
         con.close()  
 
-        m_ma_1all.Crawler(year,count,'k')
+        # m_ma_1all.Crawler(year,count,'k')
 
 elif choice == "축구 승무패":
 
@@ -153,33 +153,33 @@ elif choice == "야구 승1패":
     con.close()  
 
     if pagebb == "1경기":
-        m_bb_1all.Crawler(year,count,1) 
+        mt_so_1all.Crawler(year,count,1) 
     elif pagebb == "2경기":   
-        m_bb_1all.Crawler(year,count,2)
+        mt_so_1all.Crawler(year,count,2)
     elif pagebb == "3경기": 
-        m_bb_1all.Crawler(year,count,3)
+        mt_so_1all.Crawler(year,count,3)
     elif pagebb == "4경기": 
-        m_bb_1all.Crawler(year,count,4)
+        mt_so_1all.Crawler(year,count,4)
     elif pagebb == "5경기": 
-        m_bb_1all.Crawler(year,count,5)
+        mt_so_1all.Crawler(year,count,5)
     elif pagebb == "6경기": 
-        m_bb_1all.Crawler(year,count,6)
+        mt_so_1all.Crawler(year,count,6)
     elif pagebb == "7경기": 
-        m_bb_1all.Crawler(year,count,7)
+        mt_so_1all.Crawler(year,count,7)
     elif pagebb == "8경기": 
-        m_bb_1all.Crawler(year,count,8)
+        mt_so_1all.Crawler(year,count,8)
     elif pagebb == "9경기": 
-        m_bb_1all.Crawler(year,count,9)
+        mt_so_1all.Crawler(year,count,9)
     elif pagebb == "10경기": 
-        m_bb_1all.Crawler(year,count,10)
+        mt_so_1all.Crawler(year,count,10)
     elif pagebb == "11경기": 
-        m_bb_1all.Crawler(year,count,11)
+        mt_so_1all.Crawler(year,count,11)
     elif pagebb == "12경기": 
-        m_bb_1all.Crawler(year,count,12)
+        mt_so_1all.Crawler(year,count,12)
     elif pagebb == "13경기": 
-        m_bb_1all.Crawler(year,count,13)
+        mt_so_1all.Crawler(year,count,13)
     elif pagebb == "14경기": 
-        m_bb_1all.Crawler(year,count,14)
+        mt_so_1all.Crawler(year,count,14)
         
 elif choice == "농구 승5패":
     pagebk = st.sidebar.radio("농구 승5패", ["1경기", "2경기", "3경기", "4경기", "5경기", "6경기", "7경기",
@@ -197,33 +197,33 @@ elif choice == "농구 승5패":
     con.close()  
 
     if pagebk == "1경기":
-        m_bk_1all.Crawler(year,count,1) 
+        mt_so_1all.Crawler(year,count,1) 
     elif pagebk == "2경기":   
-        m_bk_1all.Crawler(year,count,2)
+        mt_so_1all.Crawler(year,count,2)
     elif pagebk == "3경기": 
-        m_bk_1all.Crawler(year,count,3)
+        mt_so_1all.Crawler(year,count,3)
     elif pagebk == "4경기": 
-        m_bk_1all.Crawler(year,count,4)
+        mt_so_1all.Crawler(year,count,4)
     elif pagebk == "5경기": 
-        m_bk_1all.Crawler(year,count,5)
+        mt_so_1all.Crawler(year,count,5)
     elif pagebk == "6경기": 
-        m_bk_1all.Crawler(year,count,6)
+        mt_so_1all.Crawler(year,count,6)
     elif pagebk == "7경기": 
-        m_bk_1all.Crawler(year,count,7)
+        mt_so_1all.Crawler(year,count,7)
     elif pagebk == "8경기": 
-        m_bk_1all.Crawler(year,count,8)
+        mt_so_1all.Crawler(year,count,8)
     elif pagebk == "9경기": 
-        m_bk_1all.Crawler(year,count,9)
+        mt_so_1all.Crawler(year,count,9)
     elif pagebk == "10경기": 
-        m_bk_1all.Crawler(year,count,10)
+        mt_so_1all.Crawler(year,count,10)
     elif pagebk == "11경기": 
-        m_bk_1all.Crawler(year,count,11)
+        mt_so_1all.Crawler(year,count,11)
     elif pagebk == "12경기": 
-        m_bk_1all.Crawler(year,count,12)
+        mt_so_1all.Crawler(year,count,12)
     elif pagebk == "13경기": 
-        m_bk_1all.Crawler(year,count,13)
+        mt_so_1all.Crawler(year,count,13)
     elif pagebk == "14경기": 
-        m_bk_1all.Crawler(year,count,14)
+        mt_so_1all.Crawler(year,count,14)
         
 elif choice == "경기 통계":
     pagedt = st.sidebar.radio("경기통계", ["승무패 경기통계", "승무패 배당통계", "승1패 경기통계", "승1패 배당통계", 
@@ -231,17 +231,17 @@ elif choice == "경기 통계":
     
     indegree = 30
     if pagedt == "승무패 경기통계":
-        m_dt_1all.Crawler("so1",indegree) 
+        mt_so_1all.Crawler("so1",indegree) 
     elif pagedt == "승무패 배당통계":   
-        m_dt_1all.Crawler("so2",indegree)
+        mt_so_1all.Crawler("so2",indegree)
     elif pagedt == "승1패 경기통계": 
-        m_dt_1all.Crawler("bb1",indegree)
+        mt_so_1all.Crawler("bb1",indegree)
     elif pagedt == "승1패 배당통계": 
-        m_dt_1all.Crawler("bb2",indegree)
+        mt_so_1all.Crawler("bb2",indegree)
     elif pagedt == "승5패 경기통계": 
-        m_dt_1all.Crawler("bk1",indegree)
+        mt_so_1all.Crawler("bk1",indegree)
     elif pagedt == "승5패 배당통계": 
-        m_dt_1all.Crawler("bk2",indegree)
+        mt_so_1all.Crawler("bk2",indegree)
         
 elif choice == "조합기": 
 
@@ -261,7 +261,7 @@ elif choice == "조합기":
 
         con.close() 
 
-        m_jo_1all.Crawler(year,count,'s') 
+        mt_so_1all.Crawler(year,count,'s') 
 
     elif pagejo == "야구 승1패": 
 
@@ -276,7 +276,7 @@ elif choice == "조합기":
 
         con.close()  
 
-        m_jo_1all.Crawler(year,count,'b')
+        mt_so_1all.Crawler(year,count,'b')
 
     elif pagejo == "농구 승5패": 
 
@@ -291,7 +291,7 @@ elif choice == "조합기":
 
         con.close() 
 
-        m_jo_1all.Crawler(year,count,'k')
+        mt_so_1all.Crawler(year,count,'k')
 
 else:
     st.write("메뉴를 선택하세요")
