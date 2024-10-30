28f740597ba72ca467507eeb1e8cbe1087e7aa4b
choiheebak <149461285+choiheebak@users.noreply.github.com>
2024-10-31 00:59:36 +0900

    Add files via upload

--- mt_jo_1all.py
+++ mt_jo_1all.py
@@ -15,24 +15,17 @@ def Crawler(yearc,countc,gubun):
         
     if gubun == 's':
       
-        st.subheader("축구 승무패") 
-        st.markdown(str(year)+"년 "+str(count)+"회차")
-  
+        st.subheader("축구 승무패 "+ str(year)+"년 "+str(count)+"회차") 
+    
         option = st.radio(
         "조합 선택하세요",
         ["다득표","1000원", "2000원", "3000원", "4000원", "6000원", "8000원", "9000원", "12000원", "16000원", "18000원", "24000원", 
         "27000원", "32000원", "36000원", "48000원", "54000원", "64000원", "72000원", "81000원", "96000원"],horizontal=True,
         )
  
-        # option = st.selectbox(
-        # "선택하세요", 
-        # ("다득표","1000원", "2000원", "3000원", "4000원", "6000원", "8000원", "9000원", "12000원", "16000원", "18000원", "24000원", 
-        # "27000원", "32000원", "36000원", "48000원", "54000원", "64000원", "72000원", "81000원", "96000원"), 
-        # )
+        if st.button("재조합"):            
+            pass 
 
-        # st.write(option) 
-
-        # f = open('D:/datagithub/soccer/soccer_so5_johap.txt', 'r', encoding='UTF8')
         f = open('soccer_so5_johap.txt', 'r', encoding='UTF8')
       
         rdr1 = f.readlines()    
@@ -176,16 +169,17 @@ def Crawler(yearc,countc,gubun):
 
     elif gubun == 'b':
      
-        st.subheader("야구 승1패")
-        st.markdown(str(year)+"년 "+str(count)+"회차")
-   
+        st.subheader("야구 승1패 "+ str(year)+"년 "+str(count)+"회차")
+      
         option = st.radio(
         "조합 선택하세요",
         ["다득표","1000원", "2000원", "3000원", "4000원", "6000원", "8000원", "9000원", "12000원", "16000원", "18000원", "24000원", 
         "27000원", "32000원", "36000원", "48000원", "54000원", "64000원", "72000원", "81000원", "96000원"],horizontal=True,
         )
  
-        # f = open('D:/datagithub/baseball/baseball_bb5_johap.txt', 'r', encoding='UTF8')
+        if st.button("재조합"):            
+            pass 
+
         f = open('baseball_bb5_johap.txt', 'r', encoding='UTF8')
       
         rdr1 = f.readlines()    
@@ -289,29 +283,9 @@ def Crawler(yearc,countc,gubun):
             for b in range(len(big2)):
                 big.append(bigai[b])
 
-        # print(biga)
-        # print(big)
-
         johap = johap_def('b',option,biga,big)
 
-        # print(johap)
-      
-        # data = {"홈팀":[home[0],home[1],home[2],home[3],home[4],home[5],home[6],home[7],home[8],home[9],home[10],home[11],home[12],home[13]],
-        #         "원정팀":[away[0],away[1],away[2],away[3],away[4],away[5],away[6],away[7],away[8],away[9],away[10],away[11],away[12],away[13]],
-        #         "승":[win[0],win[1],win[2],win[3],win[4],win[5],win[6],win[7],win[8],win[9],win[10],win[11],win[12],win[13]],
-        #         "①":[draw[0],draw[1],draw[2],draw[3],draw[4],draw[5],draw[6],draw[7],draw[8],draw[9],draw[10],draw[11],draw[12],draw[13]],
-        #         "패":[lose[0],lose[1],lose[2],lose[3],lose[4],lose[5],lose[6],lose[7],lose[8],lose[9],lose[10],lose[11],lose[12],lose[13]],  
-        #         # "결과":[result[0],result[1],result[2],result[3],result[4],result[5],result[6],result[7],result[8],result[9],result[10],result[11],result[12],result[13]],
-        #         "조합":[johap[0],johap[1],johap[2],johap[3],johap[4],johap[5],johap[6],johap[7],johap[8],johap[9],johap[10],johap[11],johap[12],johap[13]]}
-     
-        # df = pd.DataFrame(data, 
-
-        #         index = ["1경기","2경기","3경기","4경기","5경기","6경기","7경기","8경기","9경기","10경기","11경기","12경기","13경기","14경기"],
-        #         columns=["홈팀", "원정팀","승","①","패","조합"]) 
-
-        # st.table(df)
-        # st.dataframe(df.style.applymap(color_vowel, subset=["승","무","패"]),use_container_width=True)
-  
+       
         home = [home[0],home[1],home[2],home[3],home[4],home[5],home[6],home[7],home[8],home[9],home[10],home[11],home[12],home[13]]
         away = [away[0],away[1],away[2],away[3],away[4],away[5],away[6],away[7],away[8],away[9],away[10],away[11],away[12],away[13]]
         win = [win[0],win[1],win[2],win[3],win[4],win[5],win[6],win[7],win[8],win[9],win[10],win[11],win[12],win[13]]
@@ -348,8 +322,7 @@ def Crawler(yearc,countc,gubun):
 
     elif gubun == 'k':
      
-        st.subheader("농구 승5패")
-        st.markdown(str(year)+"년 "+str(count)+"회차")
+        st.subheader("농구 승5패 "+ str(year)+"년 "+str(count)+"회차")
     
         option = st.radio(
         "조합 선택하세요",
@@ -357,7 +330,9 @@ def Crawler(yearc,countc,gubun):
         "27000원", "32000원", "36000원", "48000원", "54000원", "64000원", "72000원", "81000원", "96000원"],horizontal=True,
         )
  
-        # f = open('D:/datagithub/basketball/basketball_bk5_johap.txt', 'r', encoding='UTF8')
+        if st.button("재조합"):            
+            pass 
+        
         f = open('basketball_bk5_johap.txt', 'r', encoding='UTF8')
       
         rdr1 = f.readlines()    
@@ -461,9 +436,6 @@ def Crawler(yearc,countc,gubun):
             for b in range(len(big2)):
                 big.append(bigai[b])
 
-        # print(biga)
-        # print(big)
-        # print(biga, big, bigai)
         johap = johap_def('k',option,biga,big)
 
         home = [home[0],home[1],home[2],home[3],home[4],home[5],home[6],home[7],home[8],home[9],home[10],home[11],home[12],home[13]]
@@ -521,8 +493,6 @@ def johap_def(gubun, option, biga, big):
     t5 = t[4]
     t6 = t[5]
    
-    # print(t1, t2, t3, t4, t5, t6)
-
     if option == "다득표": 
         for i in range(14):
             a = biga[i][:1]   
@@ -532,7 +502,6 @@ def johap_def(gubun, option, biga, big):
     elif option == "1000원": 
         for i in range(14):
             a = random.sample(big[i],1)
-            # a = random.choice(big[i])
                 
             list_johap.append(a)
 
